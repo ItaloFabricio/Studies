a4316907612d63f9c8176b13ac805196b6d2f4bb
ítalo Fabrício <italofps65@gmail.com>
2023-04-05 10:44:43 -0300

    feat: lendo a lista de usuários utilizando a API

--- ProjetoCRUD/script.js
+++ ProjetoCRUD/script.js
@@ -1,3 +1,101 @@
+fetch("https://63442914dcae733e8fd8e3e5.mockapi.io/usuarios")
+  .then(function(response) {
+      if (!response.ok) {
+          throw "Requisição chegou no servidor, mas servidor retornou com erro: " + response.statusText
+      }
+      return response.json()
+  })
+  .then(function(usuarios) {
+      console.log(usuarios)
+      let tagTabelaUsuarios = document.querySelector('#tabelaUsuarios')
+      usuarios.forEach(usuario => {
+        let tagTr = document.createElement('tr')
+        let tagTdNome = document.createElement('td')
+        tagTdNome.textContent = usuario.nome;
+        let tagTdEmail = document.createElement('td')
+        tagTdEmail.textContent = usuario.email
+
+        let tagTdOpcoes = document.createElement('td')
+
+        let tagButtonEditar = document.createElement('button')
+        let tagImgEditar = document.createElement('img')
+        tagImgEditar.src = 'img/edit.png'
+        tagImgEditar.alt = 'Editar usuário'
+        tagButtonEditar.appendChild(tagImgEditar)
+
+        let tagButtonRemover = document.createElement('button')
+        let tagImgRemover = document.createElement('img')
+        tagImgRemover.src = 'img/delete.png'
+        tagImgRemover.alt = 'Remover usuário'
+        tagButtonRemover.appendChild(tagImgRemover)
+
+        tagTdOpcoes.appendChild(tagButtonEditar)
+        tagTdOpcoes.appendChild(tagButtonRemover)
+
+
+        tagTr.appendChild(tagTdNome)
+        tagTr.appendChild(tagTdEmail)
+        tagTr.appendChild(tagTdOpcoes)
+
+        tagTabelaUsuarios.appendChild(tagTr)
+      })
+  })
+  .catch(function(error) {
+      console.log(error)
+  })
+
+carregarUsuarios();
+
+
+function carregarUsuarios(){
+  fetch("https://63442914dcae733e8fd8e3e5.mockapi.io/usuarios")
+  .then(function(response) {
+      if (!response.ok) {
+          throw "Requisição chegou no servidor, mas servidor retornou com erro: " + response.statusText
+      }
+      return response.json()
+  })
+  .then(function(usuarios) {
+      console.log(usuarios)
+      let tagTabelaUsuarios = document.querySelector('#tabelaUsuarios')
+      tagTabelaUsuarios.innerHTML = ""
+      usuarios.forEach(usuario => {
+        let tagTr = document.createElement('tr')
+        let tagTdNome = document.createElement('td')
+        tagTdNome.textContent = usuario.nome;
+        let tagTdEmail = document.createElement('td')
+        tagTdEmail.textContent = usuario.email
+
+        let tagTdOpcoes = document.createElement('td')
+
+        let tagButtonEditar = document.createElement('button')
+        let tagImgEditar = document.createElement('img')
+        tagImgEditar.src = 'img/edit.png'
+        tagImgEditar.alt = 'Editar usuário'
+        tagButtonEditar.appendChild(tagImgEditar)
+
+        let tagButtonRemover = document.createElement('button')
+        let tagImgRemover = document.createElement('img')
+        tagImgRemover.src = 'img/delete.png'
+        tagImgRemover.alt = 'Remover usuário'
+        tagButtonRemover.appendChild(tagImgRemover)
+
+        tagTdOpcoes.appendChild(tagButtonEditar)
+        tagTdOpcoes.appendChild(tagButtonRemover)
+
+
+        tagTr.appendChild(tagTdNome)
+        tagTr.appendChild(tagTdEmail)
+        tagTr.appendChild(tagTdOpcoes)
+
+        tagTabelaUsuarios.appendChild(tagTr)
+      })
+  })
+  .catch(function(error) {
+      console.log(error)
+  })
+}
+
 let tagformularioCadastroUsuario = document.querySelector(
   "#formularioCadastroUsuario"
 );
@@ -41,7 +139,8 @@ tagformularioCadastroUsuario.addEventListener("submit", function (event) {
         let tagerrorMsg = document.querySelector('#errorMsg');
         tagerrorMsg.textContent = '';
         tagerrorMsg.style.display = 'none';
-        event.target.reset;
+        event.target.reset();
+        carregarUsuarios();
       })
       .catch(function (error) {
         console.log(error);
